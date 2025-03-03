85a761f03f477a7a9e384571d89438896bec9cf7
Fearlessthegoat <bray33bray@gmail.com>
2025-03-02 22:52:36 -0500

    mainloader.lua

--- mainloader.lua
+++ mainloader.lua
@@ -1,5 +1,3 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/elfcodes808/shadowzscript/refs/heads/main/Shrimp%20game"))()
-
 -- Load Rayfield UI Library
 getgenv().SecureMode = true
 local Rayfield = loadstring(game:HttpGet("https://sirius.menu/rayfield"))()
